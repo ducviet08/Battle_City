9551554115a9642ff9b0a35ce08306b69b151f9c
ducviet08 <vietlonghieu2006@gmail.com>
2025-03-29 21:47:45 +0700

    Create PlayerTank.h

---
+++ PlayerTank.h
@@ -0,0 +1,25 @@
+#ifndef PLAYERTANK_H
+#define PLAYERTANK_H
+
+#include <SDL.h>
+#include <vector>
+#include "Bullet.h"
+
+class Wall; // Forward declaration
+
+class PlayerTank {
+public:
+    int x, y;
+    int dirX, dirY;
+    SDL_Rect rect;
+    std::vector<Bullet> bullets;
+    int cooldown;
+
+    PlayerTank(int startX, int startY);
+    void move(int dx, int dy, const std::vector<Wall> &walls);
+    void shoot();
+    void updateBullets();
+    void render(SDL_Renderer *renderer, SDL_Texture *playerTexture);
+};
+
+#endif
