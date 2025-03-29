f1bbe5be6988a1714ac3a96a0547f2c7e31216c0
ducviet08 <vietlonghieu2006@gmail.com>
2025-03-29 21:47:36 +0700

    Update Game.h

---
+++ Game.h
@@ -0,0 +1,32 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <vector>
+#include "Wall.h"
+#include "PlayerTank.h"
+#include "EnemyTank.h"
+#include "Explosion.h"
+#include <SDL_mixer.h>
+
+extern PlayerTank player;
+
+class Game {
+public:
+    Game();
+    ~Game();
+    void run();
+    void Events();
+    void update();
+    void render();
+private:
+    void generateWalls();
+    void spawnEnemies();
+    void updateEnemyTanks();
+    SDL_Texture *createScoreTexture();
+    SDL_Texture *createTimeTexture();
+};
+
+#endif
